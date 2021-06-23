0c931e0da2a3dc4b5a7a90b830c4497e25bdf591
yaufaniadam1 <yaufaniadam@gmail.com>
2021-06-23 09:21:58 +0700

    2306

--- application/modules/admin/controllers/Dashboard.php
+++ application/modules/admin/controllers/Dashboard.php
@@ -1,16 +1,11 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once "vendor/autoload.php";
-
-use PHPMailer\PHPMailer\PHPMailer;
-
 class Dashboard extends Admin_Controller
 {
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model('pengajuan_model');
-		$this->load->library('mailer');
 	}
 
 	public function index()
@@ -28,39 +23,5 @@ class Dashboard extends Admin_Controller
 		$data['title'] = 'Dashboard';
 		$data['view'] = 'dashboard/index';
 		$this->load->view('layout/layout', $data);
-	}
-
-	public function sendmails() {
-		echo "kirim email";
-
-		
-
-		$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-
-    $mail->From = 'igalowbgt@gmail.com';
-    $mail->FromName = 'adam';
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'igalowbgt@gmail.com';                     // SMTP username
-    $mail->Password   = 'Nining15';
-    // $mail->Password   = decrypt_url($this->get_settings('password_email'));
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    $mail->setFrom('igalowbgt@gmail.com', 'Adam');
-    $mail->isHTML(true);
-
-		$mail->addAddress('yaufani@gmail.com');
-
-		$mail->Subject = "hellow";
-		$mail->Body = "ini body email";
-
-		$mail->send();
-
-		$mail->ClearAddresses();
-
-
 	}
 }
