4e89d2a98c146deb91d697e8dfbf68cce402df80
yaufaniadam <yaufani@gmail.com>
2022-01-20 01:04:16 +0700

    dashboard

--- application/modules/admin/controllers/Dashboard.php
+++ application/modules/admin/controllers/Dashboard.php
@@ -6,7 +6,7 @@ class Dashboard extends Admin_Controller
 	{
 		parent::__construct();
 		$this->load->model('pengajuan_model');
-		// echo check merge
+		// echo check merged
 	}
 
 	public function index()
